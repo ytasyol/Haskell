468f3e84044e7f15e9582aad314a6597303f4063
ytasyol <yuecel.tasyol@gmail.com>
2015-03-06 23:09:36 +0100

    BinaryTreeModul um Funktionen erweitert

--- src/main.hs
+++ src/main.hs
@@ -1,10 +1,13 @@
 module Main where
 
---import BinaryTreeModul
-import Baum
+import BinaryTreeModul
+--import Baum
 
---myBTree :: BinaryTree Int
---myBTree = Node 1 (Node 2 Nil Nil) (Node 3 Nil Nil)
+myBTree1 :: BinaryTree Int
+myBTree1 = Node 3 (Node 4 Nil Nil) (Node 5 Nil Nil)
+myBTree2 :: BinaryTree Int
+myBTree2 = Node 1 (Node 2 Nil Nil) (Node 3 Nil Nil)
 
-baum :: Baum Int
-baum = Blatt Nil 1 Nil
+
+--baum :: Baum Int
+--baum = Blatt Nil 1 Nil
