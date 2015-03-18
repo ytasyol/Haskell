d83b49a71d41e02ca8c9f86c4bea630b97ad70b2
ytasyol <yuecel.tasyol@gmail.com>
2015-03-18 20:41:00 +0100

    refactoring

--- src/AVLTree.hs
+++ src/AVLTree.hs
@@ -9,28 +9,39 @@ type AVLTree = BST.BinarySearchTree
 
 rebalance :: AVLTree a -> AVLTree a
 rebalance BST.Nil = BST.Nil
-rebalance (BST.Node tElement tLeft tRight)
-    | (<) (abs (heightDiffChilds (BST.Node tElement tLeft tRight))) 2 = (BST.Node tElement tLeft tRight)
-    | (>) (abs (heightDiffChilds tLeft)) 1 = (BST.Node tElement (rebalance tLeft) tRight)
-    | (>) (abs (heightDiffChilds tRight)) 1 = (BST.Node tElement tLeft (rebalance tRight))
-    | (<) (BST.height tLeft) (BST.height tRight) && (<) (heightDiffChilds tRight) 0 =
-        rotateLeft(BST.Node tElement tLeft tRight)
-    | (>) (BST.height tLeft) (BST.height tRight) && (>) (heightDiffChilds tLeft) 0 =
-        rotateRight (BST.Node tElement tLeft tRight)
-    | (<) (BST.height tLeft) (BST.height tRight) && (>) (heightDiffChilds tRight) 0 =
-        rotateRL (BST.Node tElement tLeft tRight)
-    | (>) (BST.height tLeft) (BST.height tRight) && (<) (heightDiffChilds tLeft) 0 =
-        rotateLR (BST.Node tElement tLeft tRight)
-    | otherwise = (BST.Nil)
+rebalance (BST.Node tE tL tR)
+    | (>) (abs (hDC tL)) 1 = (BST.Node tE (rebalance tL) tR)
+    | (>) (abs (hDC tR)) 1 = (BST.Node tE tL (rebalance tR))
+    | (<) (BST.height tL) (BST.height tR) && (<) (hDC tR) 0 = rL(BST.Node tE tL tR)
+    | (>) (BST.height tL) (BST.height tR) && (>) (hDC tL) 0 = rR (BST.Node tE tL tR)
+    | (<) (BST.height tL) (BST.height tR) && (>) (hDC tR) 0 = rRL (BST.Node tE tL tR)
+    | (>) (BST.height tL) (BST.height tR) && (<) (hDC tL) 0 = rLR (BST.Node tE tL tR)
+    | otherwise = (BST.Node tE tL tR)
     where
-        heightDiffChilds BST.Nil = 0
-        heightDiffChilds (BST.Node _ tLeft tRight) = ((-) (BST.height tLeft) (BST.height tRight))
-        rotateRL (BST.Node tElement tLeft tRight) = rotateLeft (BST.Node tElement tLeft (rotateRight tRight))
-        rotateLR (BST.Node tElement tLeft tRight) = rotateRight (BST.Node tElement (rotateLeft tLeft) tRight)
-        rotateLeft (BST.Node tElement tLeft (BST.Node ctElement ctLeft ctRight)) =
-            BST.Node ctElement (BST.Node tElement tLeft ctLeft) ctRight
-        rotateRight (BST.Node tElement (BST.Node ctElement ctLeft ctRight) tRight) =
-            BST.Node ctElement ctLeft (BST.Node tElement ctRight tRight)
+        hDC = heightDiffChilds
+        rRL = rotateRL
+        rLR = rotateLR
+        rL = rotateLeft
+        rR = rotateRight
+
+heightDiffChilds :: AVLTree e -> Int
+heightDiffChilds BST.Nil = 0
+heightDiffChilds (BST.Node _ tLeft tRight) = ((-) (BST.height tLeft) (BST.height tRight))
+
+rotateRL :: AVLTree e -> AVLTree e
+rotateRL (BST.Node tElement tLeft tRight) = rotateLeft (BST.Node tElement tLeft (rotateRight tRight))
+
+rotateLR :: AVLTree e -> AVLTree e
+rotateLR (BST.Node tElement tLeft tRight) = rotateRight (BST.Node tElement (rotateLeft tLeft) tRight)
+
+rotateLeft :: AVLTree e -> AVLTree e
+rotateLeft (BST.Node tElement tLeft (BST.Node ctElement ctLeft ctRight)) =
+    BST.Node ctElement (BST.Node tElement tLeft ctLeft) ctRight
+
+rotateRight :: AVLTree e -> AVLTree e
+rotateRight (BST.Node tElement (BST.Node ctElement ctLeft ctRight) tRight) =
+    BST.Node ctElement ctLeft (BST.Node tElement ctRight tRight)
+
 
 insert :: (Ord a, Eq a) => a -> AVLTree a -> AVLTree a
 insert pElement pTree = rebalance (BST.insert pElement pTree)
