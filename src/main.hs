44b7f8769d522b6b742f5e70dfb4524726283459
ytasyol <yuecel.tasyol@gmail.com>
2015-03-21 17:17:52 +0100

    create GUI with Glade and first steps with Gtk and Haskell

--- src/main.hs
+++ src/main.hs
@@ -1,5 +1,5 @@
 module Main where
-
+{-
 import Test.HUnit
 import Test.HUnit.Text
 import BinarySearchTreeTest
@@ -10,3 +10,36 @@ main = do
     runTestTT bstTestList
     putStrLn "AVLTreeTest:"
     runTestTT avlTestList
+-}
+
+{-Imports-}
+import AVLTree
+{-GUI Imports-}
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Builder
+import System.Glib.Attributes {-get, set-}
+
+avlTree = Nil
+
+main = do
+    initWindow
+
+initWindow = do
+    initGUI
+    --Creat builder and load glade file
+    builder <- builderNew
+    builderAddFromFile builder "interface2.glade"
+    mainWindow <- builderGetObject builder castToWindow "windowMain"
+    onDestroy mainWindow mainQuit
+    --Label, Inputs and there Events
+    treeView <- builderGetObject builder castToLabel "labelView"
+    --Button and ButtonEvents
+    isEmptyButton <- builderGetObject builder castToButton "buttonIsEmpty"
+    onClicked isEmptyButton (set treeView [labelLabel := (show (insert 5 avlTree))])
+
+    widgetShowAll mainWindow
+    mainGUI
+
+--isEmptyClick = do
+    --(putStrLn (show (insert 5 avlTree)))
+    --set treeView [labelLabel := (show (insert 5 avlTree))]
